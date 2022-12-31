8162f8d7d93e0b28345af4e90574b9571d6f90ff
nathan <nlai274@gmail.com>
2022-12-30 16:52:39 -0800

    working play command

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ https://discord.com/oauth2/authorize?client_id=617027534042693664&scope=bot
 // =============================================================
 const Discord = require("discord.js");
 const firebase = require("firebase-admin");
+const distube = require("distube");
 
 const FIREBASE_AUTH = require("./firebase.json");
 
@@ -18,7 +19,8 @@ const client = new Discord.Client({
   intents:[
     "Guilds",
     "GuildMessages",
-    "MessageContent"
+    "MessageContent",
+    "GuildVoiceStates"
 ]
 });
 
@@ -42,115 +44,50 @@ firebase.database().ref('global').once('value').then((snapshot) => {
     console.log("Global config:\n", global.config);
   }
 
+  // DISTUBE INTEGRATION
+  client.distube = new distube.DisTube(client, global.config.distube);
+
+  // READY
+  client.on("ready", () => {
+    console.log("Bot connected!");
+    client.user.setPresence({
+      activities: [{name: 'Praising Zeta'}],
+      status: 'online'
+    });
+  });
+  
+  // MESSAGE HANDLER
+  client.on("messageCreate", (message) => {
+    if (message.author.bot || 
+        message.channel.type == 'dm' ||
+        !message.content.toLowerCase().startsWith(global.config.prefix))
+          return;
+    
+    const args = message.content.slice(global.config.prefix.length).trim().split(/ +/g);
+    switch(args.shift().toLowerCase()){
+      case "play":
+        client.distube.play(message.member.voice.channel, args.join(" "), {
+          member: message.member,
+          textChannel: message.channel,
+          message
+        });
+        break;
+      default:
+        break;
+    }
+  });
+
+  client.distube.on("playSong", (queue, song) => {
+    let nowPlaying = "NOW PLAYING: " + song.name;
+    queue.textChannel.send(nowPlaying);
+    if(debugMode)
+      console.log(nowPlaying);
+  });
+
   // BOT LOGIN
   try {
-    client.login(global.auth).then(() => console.log("Bot connected."));
+    client.login(global.auth);
   } catch (error) {
     console.log(error);
   }
 });
-
-/*
-// =============================================================
-// CONFIG VARIABLES
-// =============================================================
-const prefix = config.main.prefix;
-const debugMode = config.main.debugMode;
-
-// =============================================================
-// MISC GLOBAL VARIABLES
-// =============================================================
-const currDate = new Date();
-
-// =============================================================
-// INTIALIZING CLIENT
-// =============================================================
-const client = new Discord.Client({
-    intents:[
-      "Guilds",
-      "GuildMessages",
-      "MessageContent"
-  ]
-});
-
-// Creates a collection intended to store commands
-client.commands = new Discord.Collection();
-
-// Creates a list intended to store a queue for music
-client.queue = {
-  data: {},
-  order: [],
-  current: "",
-  loop: false
-};
-
-// Reads every file in the commands folder and creates an array of them
-const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
-
-// Adds the array of commands to the commands collection
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
-}
-
-// Connecting the bot after initializing all other modules
-client.on("ready", () => {
-  console.log("Bot connected!");
-  client.user.setActivity("nathan", { type: "ACTIVE" });
-  console.log(config);
-});
-
-// =============================================================
-// COMMAND AND MESSAGE CONTROL
-// =============================================================
-client.on("message", (msg) => {
-
-  // Ignore these messages
-  if (msg.author.bot ||
-    msg.channel.type == "dm")
-    return;
-
-  // Messages without a prefix are ignored
-  if (!msg.content.startsWith(prefix)) return;
-
-  // COMMAND HANDLER
-  // Handles all message commands and arguments
-
-  // Removes prefix and splits message into command and arguments
-  const args = msg.content.slice(prefix.length).trim().split(" ");
-  const command = args.shift().toLowerCase();
-
-  // Dynamic command handler using commands stored in commands folder
-  try {
-    if (client.commands.get(command).database) {
-      client.commands.get(command).execute(msg, args, database);
-    }
-    if (client.commands.get(command).memory) {
-      client.commands.get(command).execute(msg, args, client);
-    }
-    else {
-      client.commands.get(command).execute(msg, args);
-    }
-  } catch (error) {
-    console.log("Invalid command!");
-    console.log(error);
-  } finally {
-    // Logs any commands with information in the console
-    if (debugMode) {
-      console.log(msgInfo(msg));
-    }
-  }
-});
-
-// =============================================================
-// BOT LOGIN
-// =============================================================
-try {
-  client.login(auth.token);
-} catch (error) {
-  console.log(error);
-}
-
-*/
