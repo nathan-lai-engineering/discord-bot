063b380a8c3ec96c030459ad277d553c55e2cb0a
nathan <nlai274@gmail.com>
2023-09-27 00:38:49 -0700

    working outro play

--- commands/outro.js
+++ commands/outro.js
@@ -16,14 +16,14 @@ module.exports = {
                     option
                         .setName('url')
                         .setDescription('youtube url'))
-                .addStringOption(option => 
+                .addNumberOption(option => 
                     option
                         .setName('start')
-                        .setDescription('timestamp'))
-                .addStringOption(option => 
+                        .setDescription('start time in seconds'))
+                .addNumberOption(option => 
                     option
-                        .setName('end')
-                        .setDescription('timestamp, max 20 secs')))
+                        .setName('duration')
+                        .setDescription('max 20')))
         .addSubcommand(subcommand =>
             subcommand
                 .setName('toggle')
@@ -31,18 +31,31 @@ module.exports = {
 	async execute(interaction) {
         switch(interaction.options.getSubcommand()){
             case 'play':
+                this.playOutro(interaction);
+                interaction.reply({content: 'Your outro is playing, gg gn', ephemeral: true});
                 break;
 
 
             case 'set':
                 let url = interaction.options.getString('url');
-                let start = interaction.options.getString('start');
-                let end = interaction.options.getString('end');
+                let start = interaction.options.getNumber('start');
+                if(start < 0){
+                    interaction.reply({content: 'Start is atleast 0 seconds...', ephemeral: true});
+                    return;
+                }
+
+
+                let duration = interaction.options.getNumber('duration');
+                if(duration <= 0 || duration > 20){
+                    interaction.reply({content: 'Duration is atleast 0 seconds and at most 20 seconds', ephemeral: true});
+                    return;
+                }
+
                 let toggle = true;
                 let outroData = {
                     url: url,
                     start: start,
-                    end: end,
+                    duration: duration,
                     toggle: toggle};
 
                 interaction.client.db.collection('users').doc(interaction.user.id).set({'outro': outroData}, {merge: true})
@@ -72,4 +85,56 @@ module.exports = {
                 interaction.reply('this should never happen');
         }
 	},
+
+    async playOutro(interaction){
+        const distube = interaction.client.distube;
+        interaction.client.db.collection('users').doc(interaction.user.id).get().then(snapshot => {
+            let outroData = snapshot.data().outro;
+
+            if(outroData == null || outroData.url == null || outroData.start == null || outroData.duration == null){
+                interaction.reply({content:'You do not have an outro set up my guy', ephemeral: true});
+                return;
+            }
+
+            let currentQueue = distube.queues.get(interaction.guild);
+
+            if(currentQueue != null && currentQueue.playing){
+         
+            }
+            else {
+                playSongPriority(outroData.url, interaction);
+                delayedSkip(outroData, interaction);
+            }
+
+        });
+    }
 };
+
+function playSongPriority(url, interaction){
+    if(!interaction.member.voice.channel){
+        if(interaction.client.debugMode)
+            console.log("User not in a voice channel!");
+        interaction.reply("Shouldn't you be in a voice channel?");
+        return;
+    }
+
+    interaction.client.distube.play(interaction.member.voice.channel, url, {
+        member: interaction.member,
+        textChannel: interaction.channel,
+        position: 1
+    }); 
+};
+
+function delayedSkip(outroData, interaction){
+    const distube = interaction.client.distube;
+    const currentQueue = distube.queues.get(interaction.guild);
+
+    distube.addSongFunctions.push(
+        function(){setTimeout(()=>{
+            if(currentQueue != null && currentQueue.songs.length == 1)
+                distube.clear(interaction.guild);
+            else
+                distube.skip(interaction.guild);
+        }, 
+            outroData.duration * 1000)});
+}
--- distube_events.js
+++ distube_events.js
@@ -1,4 +1,6 @@
 const { DisTube } = require('distube');
+const Discord = require("discord.js");
+const {playOutro} = require('./commands/outro.js');
 
 function createEvent(client, event, eventText){
     client.distube.on(event, (queue, song) => {
@@ -9,11 +11,33 @@ function createEvent(client, event, eventText){
     });
 }
 
+function createPlaySongEvent(client){
+    client.distube.on("playSong", (queue, song) => {
+        replyMessage = "NOW PLAYING: " + String(song.name);
+        queue.textChannel.send(replyMessage);
+        if(client.debugMode)
+          console.log(replyMessage);
+        if(client.distube.addSongFunctions.length > 0){
+            console.log('next add song function');
+            client.distube.addSongFunctions.pop()();
+        };
+    });
+}
+
 exports.load = (client, disConfig) => {
     client.distube = new DisTube(client, disConfig);
     client.enabledModules.push("distube");
+    client.distube.addSongFunctions = [];
 
-    createEvent(client, "playSong", "NOW PLAYING: ");
+    createPlaySongEvent(client);
     createEvent(client, "addSong", "ADDED SONG TO QUEUE: ");
+    //createEvent(client, "playSong", "NOW PLAYING: ");
     createEvent(client, "searchNoResult", "COULD NOT FIND SONG: ");
+
+    client.on(Discord.Events.VoiceStateUpdate, (oldState, newState) => {
+        let guildid = oldState.guild.id;
+        if(client.voice.adapters.guildid.channelId == oldState.channelId && oldState.channelId != newState.channelId){
+            playOutro();
+        }
+    });
 }
