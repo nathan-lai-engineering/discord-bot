6e256890088bd4efd6da278dc120f537f8d8846f
nathan <nlai274@gmail.com>
2023-09-29 23:41:37 -0700

    log change

--- utils/log.js
+++ utils/log.js
@@ -1,9 +1,12 @@
 const {humanTimeNow} = require('./time.js');
 
 module.exports = {
-    logDebug(debugMode, message){
-        if(debugMode)
+    logDebug(client, message){
+        if(client.debugMode)
             return console.log('[%s] %s', humanTimeNow(), message);
         return;
+    },
+    log(message){
+        return console.log('[%s] %s', humanTimeNow(), message);
     }
 }
