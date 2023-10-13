539a027411daf0ccf326e95ffae676570a03b6bb
nathan <nlai274@gmail.com>
2023-10-13 02:17:49 -0700

    working call to firestore

--- riot_module.js
+++ riot_module.js
@@ -1,11 +1,57 @@
 const {log, logDebug} = require('./utils/log.js');
 const Discord = require("discord.js");
 const axios = require('axios')
+const firebase = require("firebase-admin");
 
 exports.load = (client, apiKey) => {
     logDebug(client, 'Loading riot module');
     client.enabledModules.push("riot");
 
-    let interval = 5 * 60 * 1000;
+    let interval = 10 * 60 * 1000;
+
+
+
+    let checkRiotData = () => {
+        setTimeout(checkRiotData, interval);
+        logDebug(client, 'Performing check on Riot Web API');
+
+        getPUUIDs(client).then(puuids => {
+            console.log(puuids);
+            for(index in puuids){
+                let discordId = puuids[index].id;
+                let riotId = puuids[index].puuid;
+
+                
+            }
+        })
+
+
+    };
+    checkRiotData();
+    setTimeout(checkRiotData, interval);
 }
 
+function getPUUIDs(client){
+    logDebug(client, 'Getting PUUID data from firestore');
+    
+    return client.db.collection('users').get().then(snapshot => {
+        let puuids = [];
+        snapshot.forEach(docSnapshot => {
+            if('puuid' in docSnapshot.data()){
+                puuids.push({
+                    id: docSnapshot.id,
+                    puuid: docSnapshot.data().puuid
+                });
+            }
+        });
+        return puuids;
+
+    })
+    /*
+    return client.db.collection('users').doc(member.id).get().then(snapshot => {
+        if('puuid' in snapshot.data()){
+            console.log(snapshot.data().puuid);
+        }
+    });
+    */
+}
