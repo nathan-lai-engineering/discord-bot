6ff6882248650c247b45cebaaf4e864b7ad9f1b9
nathan <nlai274@gmail.com>
2023-10-14 02:52:01 -0700

    remove console.log

--- utils/riotUtils.js
+++ utils/riotUtils.js
@@ -84,7 +84,6 @@ function timeToDate(unixTime){
     let yourDate = new Date(unixTime);
     const offset = yourDate.getTimezoneOffset(); 
     yourDate = new Date(yourDate.getTime() + (offset*60*1000)); 
-    console.log(yourDate.toISOString())
     let datatimeData = yourDate.toISOString().split('T');
     let dateData = datatimeData[0].split('-');
     let dateString = `${dateData[1]}/${dateData[2]}/${dateData[0]}`
