63141352df41ba40071f618f494a453381a92423
nathan <nlai274@gmail.com>
2023-10-17 19:07:35 -0700

    setting up register subcommand

--- commands/riot.js
+++ commands/riot.js
@@ -9,20 +9,17 @@ module.exports = {
         .addSubcommand(subcommand =>
             subcommand
                 .setName('set')
-                .setDescription('set the channel for riot games notifications')),
+                .setDescription('set the channel for riot games notifications'))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('register')
+                .setDescription('registers your Riot username to your discord'))
+                .addStringOption(option =>
+                    option
+                        .setName('summoner name')
+                        .setDescription('caps sensitive summoner name')
+                        .setRequired(true)),
 	async execute(interaction) {
-        switch(interaction.options.getSubcommand()){
-            case 'set':
-                let db = interaction.client.db;
-                db.collection('guilds').doc(interaction.guild.id).set({'channels': {'riot': interaction.channel.id}}, {merge: true}).then(() => {
-                    interaction.reply({content:'Channel set for riot notifications!', ephemeral: true});
-                    logDebug(interaction.client, `Channel ${interaction.channel.id} set as Riot Games notification channel`);
-                });
-                break;
-
-            default: 
-                break;
-        }
-
+        
 	},
 };
