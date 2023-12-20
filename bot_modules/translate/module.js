38f8ffd235ae09b7ba277228945dc72f623ec657
nathan <nlai274@gmail.com>
2023-12-20 15:54:22 -0800

    doing some fixes, api integration

--- bot_modules/translate/module.js
+++ bot_modules/translate/module.js
@@ -5,7 +5,7 @@ const deepl = require('deepl-node');
 exports.load = (client) => {
     logDebug(client, 'Loading Translate module');
     client.on(Discord.Events.MessageCreate, async message => {
-        if(!client.apiKeys && message.author.bot)
+        if(!client.apiKeys || message.author.bot)
           return;
  
         const translator = new deepl.Translator(client.apiKeys['deepl']);
@@ -13,8 +13,8 @@ exports.load = (client) => {
         .then(res => {
             console.log(res);
             if(res){
-                if(res.detected_source_language && !res.detected_source_language.toLowerCase().includes("en") && res.text){
-                    message.reply(`From ${res.detected_source_language}: ${res.text}`).catch(console.error);
+                if(res.detectedSourceLang && !res.detectedSourceLang.toLowerCase().includes("en") && res.text){
+                    message.reply(`${res.detectedSourceLang} -> English: ${res.text}`).catch(console.error);
                 }
             }
         })
