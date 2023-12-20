a0a5872eef1282037996b62da71ea5af58f3004c
nathan <nlai274@gmail.com>
2023-12-19 21:13:21 -0800

    add error check for ensuring api keys are loaded in first

--- bot_modules/translate/module.js
+++ bot_modules/translate/module.js
@@ -1,19 +1,19 @@
 const {log, logDebug} = require('../../utils/log.js');
 const Discord = require("discord.js");
-import * as deepl from 'deepl-node';
+const deepl = require('deepl-node');
 
 exports.load = (client) => {
     logDebug(client, 'Loading Translate module');
     client.on(Discord.Events.MessageCreate, async message => {
-        if(message.author.bot)
+        if(!client.apiKeys && message.author.bot)
           return;
  
         const translator = new deepl.Translator(client.apiKeys['deepl']);
-        translator.translateText(message.content, null, 'en')
+        translator.translateText(message.content, null, 'en-US')
         .then(res => {
             console.log(res);
             if(res){
-                if(res.detected_source_language && !res.detected_source_language.includes("EN") && res.text){
+                if(res.detected_source_language && !res.detected_source_language.toLowerCase().includes("en") && res.text){
                     message.reply(`From ${res.detected_source_language}: ${res.text}`).catch(console.error);
                 }
             }
