e362abbbaae5aebe0730e0b5cc6f2f4e22ca4fa7
nathan <nlai274@gmail.com>
2024-04-09 19:57:37 -0700

    banking on that error condition

--- bot_modules/social_credit/commands/flowercredit.js
+++ bot_modules/social_credit/commands/flowercredit.js
@@ -375,10 +375,10 @@ async function flowerfallRanking(interaction){
         }
     }
     embed.setFooter({text:"This is an evaluation of your self-worth as a human being. -Blazeris"});
-
+    
     return interaction.fetchReply()
-        .then(reply => interaction.reply({embeds: [embed]}))
-        .catch(error => interaction.followUp({embeds: [embed]}));
+        .then(reply => interaction.followUp({embeds: [embed]}))
+        .catch(error => interaction.reply({embeds: [embed]}));
 }
 
 async function flowerfallMassReset(interaction){
