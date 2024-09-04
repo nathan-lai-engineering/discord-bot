f120f66b7edc2a1d6256997749a05df148ef0393
Nathan Lai <42527870+nathan-lai-engineering@users.noreply.github.com>
2024-09-03 21:18:25 -0700

    semi-functional buttons

--- commands/readycheck.js
+++ commands/readycheck.js
@@ -8,10 +8,9 @@ module.exports = {
 	async execute(interaction) {
         interaction.reply({content: `Creating ready check...`, ephemeral:true})
         var embed = new Discord.EmbedBuilder()
-            .setColor(0xf4d03f)
             .setTitle('Gamer Ready Check')
-            .setDescription('Waiting for all responses...')
-            .setThumbnail('https://raw.githubusercontent.com/nathan-lai-engineering/discord-bot/master/assets/yellow%20question.png')
+        
+        embed = editEmbedInProgress(embed)
 
         var ready = new Discord.ButtonBuilder()
             .setCustomId('gamerready')
@@ -26,6 +25,41 @@ module.exports = {
         var row = new Discord.ActionRowBuilder()
             .addComponents(ready, notReady)
 
-        interaction.channel.send({embeds: [embed], components: [row]})
+        const readyCheck = await interaction.channel.send({embeds: [embed], components: [row]})
+
+        try{
+            while(true){
+                const confirmation = await readyCheck.awaitMessageComponent({time: 60_000})
+
+                if(confirmation.customId === 'gamerready'){
+                    await confirmation.update({embeds: [editEmbedReady(embed)], components: [row]})
+                } else if(confirmation.customId === 'gamernotready') {
+                    await confirmation.update({embeds: [editEmbedNotReady(embed)], components: [row]})
+                }
+            }
+        } catch (e) {
+            await readyCheck.edit({embeds: [editEmbedNotReady(embed)], components: []})
+        }
     }
+}
+
+function editEmbedReady(embed){
+    return embed
+        .setColor(0x18e327)
+        .setDescription('Everyone is ready!')
+        .setThumbnail('https://raw.githubusercontent.com/nathan-lai-engineering/discord-bot/master/assets/green%20ready.png')
+}
+
+function editEmbedNotReady(embed){
+    return embed
+        .setColor(0xe32b18)
+        .setDescription('Everyone is not ready.')
+        .setThumbnail('https://raw.githubusercontent.com/nathan-lai-engineering/discord-bot/master/assets/red%20not%20ready.png')
+}
+
+function editEmbedInProgress(embed){
+    return embed
+        .setColor(0xf4d03f)
+        .setDescription('Waiting for all responses...')
+        .setThumbnail('https://raw.githubusercontent.com/nathan-lai-engineering/discord-bot/master/assets/yellow%20question.png')
 }
