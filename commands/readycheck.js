d629ceca714ce7997f4403c3a8b0bd2a7a55d233
Nathan Lai <42527870+nathan-lai-engineering@users.noreply.github.com>
2024-09-03 20:20:48 -0700

    basic ui

--- commands/readycheck.js
+++ commands/readycheck.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const Discord = require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,8 +10,22 @@ module.exports = {
         var embed = new Discord.EmbedBuilder()
             .setColor(0xf4d03f)
             .setTitle('Gamer Ready Check')
+            .setDescription('Waiting for all responses...')
             .setThumbnail('https://raw.githubusercontent.com/nathan-lai-engineering/discord-bot/master/assets/yellow%20question.png')
 
-        interaction.channel.send(embed)
+        var ready = new Discord.ButtonBuilder()
+            .setCustomId('gamerready')
+            .setLabel('I am READY')
+            .setStyle(Discord.ButtonStyle.Success)
+
+        var notReady = new Discord.ButtonBuilder()
+            .setCustomId('gamernotready')
+            .setLabel('I am NOT ready')
+            .setStyle(Discord.ButtonStyle.Danger)
+
+        var row = new Discord.ActionRowBuilder()
+            .addComponents(ready, notReady)
+
+        interaction.channel.send({embeds: [embed], components: [row]})
     }
 }
